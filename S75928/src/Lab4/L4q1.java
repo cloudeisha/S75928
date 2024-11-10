366f7617d72e7899436ae585734a5e40d3fb7637
user <user@172.17.133.121>
2024-11-10 09:31:36 +0800

    This is my first commit  for lab 4 activity 1

---
+++ S75928/src/Lab4/L4q1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class L4q1 {
+     public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+       System.out.print("Enter first integer:");
+       int num1 = input.nextInt();
+       System.out.print("Enter second integer:");
+       int num2 = input.nextInt();
+       
+       int square1= num1*num1;
+       int square2 = num2*num2;
+       int sumSquare = square1 + square2;
+       int diffSquare =square1 - square2;
+       
+       System.out.println("The square of the first integer is " + square1);
+       System.out.println("The square of the second integer is " + square2);
+       System.out.println("The sum of both square is " + sumSquare);
+       System.out.println("The difference between both square is " + diffSquare);
+    }
+}
