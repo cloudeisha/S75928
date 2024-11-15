ea9200acafdc2673a8000d070e895cacc01a69e0
user <user@172.17.133.121>
2024-11-15 16:05:14 +0800

    This is my first commit  for lab 4 activity 2

---
+++ S75928/src/Lab4/L4q2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class L4q2 {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Enter radius :");
+        float radius = Float.parseFloat(input);
+        
+        double diameter = 2*radius;
+        double circumference = 2*3.14159*radius;
+        double area = 3.14159*radius*radius;
+        String message = String.format("The diameter is %.2f",diameter)+ //%.2f= 2 floating point.GUNA COMMA,bukan "+"
+                String.format("The circumference is %.2f",circumference)+
+                String.format("The area is %.2f",area); //utk pilih how many decimal places we want.
+
+        
+        JOptionPane.showMessageDialog(null,message);//
+        JOptionPane.showMessageDialog(null,message);//
+        JOptionPane.showMessageDialog(null,message); // 3 ni JOPtion class yang biasa (display all decimal places
+    }
+}
